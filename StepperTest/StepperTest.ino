57745491a98b1c01cd5b73d162b8163ad2f2e858
Pete Milne <milnepe@gmail.com>
2018-06-20 22:29:47 +0100

    added forward & reverse

--- StepperTest/StepperTest.ino
+++ StepperTest/StepperTest.ino
@@ -5,16 +5,18 @@
 
     Driver test for Colour Sorter stepper motor.
 
-    @section  HISTORY
-
 */
 
-//Declare EasyDriver pins
+//Define EasyDriver pins
 #define stp 2    // stp pin connected to D2
 #define dir 3    // dir pin connected to GND (forwards)
 #define MS1 4    // MS1 & MS2 connected to 5V (1/8th step resoluion)
 #define MS2 5    
 #define EN  6    // EN connected to D6
+
+// Define directions
+#define REVERSE HIGH // Move in reverse4
+#define FORWARD  LOW // Move forwards
 
 // 5 equal positions for platter
 const int NUM_OF_POSITIONS = 5; 
@@ -52,6 +54,7 @@ void loop() {
       position = inString.toInt();
       Serial.println(position);
       showIndex();
+      digitalWrite(EN, LOW); //Pull enable pin low to allow motor control
       setPosition(position);
       digitalWrite(EN, HIGH); //Pull enable pin high to reduce current
       showIndex();
@@ -61,18 +64,20 @@ void loop() {
   }
 }
 
-// Recalculate position from Origin for each index after moving forwards one position
-void updateIndex(){
+// Recalculate position from Origin of each Index after moving one position
+void updateIndex(int direction){
   for(int i=0; i<NUM_OF_POSITIONS; i++){
-    // Get current position from Origin for index
-    int p = index[i];
-    // Reset current position from Origin, if already at Origin
-    if(p == 0)
-      p = NUM_OF_POSITIONS; 
-    // Decrement by one as new position is one position closer to Origin
-    p--;
-    // Update index with new position from Origin
-    index[i] = p;
+    int p = index[i]; // Get current position from Origin for index 
+    if (direction == FORWARD){
+      if(p == 0) p = NUM_OF_POSITIONS; // Reset position from Origin, if already at Origin
+      p--; // Decrement by one as new position is one position closer to Origin
+    }
+    else
+      if (direction == REVERSE) {
+      p++; // Increament by one as new position is one position further from Origin
+      if(p == NUM_OF_POSITIONS) p = 0; // Reset to Origin, if now at Origin
+    }
+    index[i] = p; // Update index with new position from Origin
   }    
 }
 
@@ -85,9 +90,10 @@ void showIndex(){
   Serial.println();  
 }
 
-// Step motor forwards one position, each position is made up of multiple micro-steps
-void stepForwardOnePosition(){
-  digitalWrite(EN, LOW); //Pull enable pin low to allow motor control
+// Step motor one position, each position is made up of multiple micro-steps
+void stepOnePosition(int direction){
+  digitalWrite(dir, direction); //Pull dir pin LOW for forwards / HIGH for reverse
+  delay(1);
   for(int i = 0; i < ONE_POSITION; i++)  // Move the number of micro-steps for each Position
   {
     digitalWrite(stp,HIGH); //Trigger one step forward
@@ -95,15 +101,22 @@ void stepForwardOnePosition(){
     digitalWrite(stp,LOW); //Pull step pin low so it can be triggered again
     delay(1);
   }
+  updateIndex(direction); // Update value of index after each change in position
 }
 
 // Move any valid index to Origin
 void setPosition(int pos){
-  if (pos >= 0 && pos < NUM_OF_POSITIONS){
+  int direction;
+  if ((pos >= 0) && (pos < NUM_OF_POSITIONS)){
     int p = index[pos];
+    if ( p > 2 ) {
+      p = NUM_OF_POSITIONS - p;
+      direction = REVERSE;
+    }
+    else
+      direction = FORWARD;    
     for (int i=0; i<p; i++){
-      stepForwardOnePosition(); // Move forwards one position at a time
-      updateIndex(); // Update value of index after each change in position
+      stepOnePosition(direction); // Move forwards one position at a time
     }
   }
   else return; // Do nothing if index is out of range
